28ed294d22560d04a7a6507d7472808afd64a7c8
Kishan Patel <kolipatelkishan786@gmail.com>
2019-02-11 19:30:22 +0530

    172 TD Submitting and Using the Form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'forms';
+  suggestUserName() {
+    const suggesteName = 'Superuser';
+  }
+
+  onsubmit(form: NgForm) {
+    console.log(form);
+  }
 }
